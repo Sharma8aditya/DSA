00f2bfe09f88753af110035de4f70a40dc0b8b92
Aditya Sharma <87682688+Sharma8aditya@users.noreply.github.com>
2022-12-28 00:12:09 +0530

    Time: 15 ms (81.22%), Space: 21.9 MB (19.72%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,28 +11,26 @@
  */
 class Solution {
 public:
-    bool helper(TreeNode* root,long low ,long high){
-        // Empty trees are valid BSTs.
-        if(root == NULL){
-            return true;
-        }
+    void inorder(TreeNode* root, vector<int>& ans){
+        if(!root) return;
         
-        // Condition of Binary Search Tree 
-        // left side node values of the root should be less value than the root and right side should be greater
-        // If that is true check recursivley for the root child's
-        
-        if((root->val < high) && (root->val > low)){
-          return (helper(root->left, low, root->val) && helper(root->right, root->val, high));
-
-        }else{
-            //if this root node violates the min/max constraint
-            return false;
-        }
-        
+        inorder(root->left, ans);
+        ans.push_back(root->val);
+        inorder(root->right, ans);
     }
-    
-    bool isValidBST(TreeNode* root) {
-        bool ans = helper(root, LONG_MIN, LONG_MAX);
-        return ans;
+    //Function to check whether a Binary Tree is BST or not.
+    bool isValidBST(TreeNode* root) 
+    {
+        vector<int> ans;
+        inorder(root, ans);
+        for(int i =0; i<ans.size()-1; i++){
+            if(ans[i] >= ans[i+1]){
+                return false;
+            }
+            
+        }
+        return true;
+        // Your code here
+        
     }
 };
