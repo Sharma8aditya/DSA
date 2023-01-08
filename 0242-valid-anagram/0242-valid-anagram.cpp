54bdca60d2ca3fae40bda312f05ac65df46f51cf
Aditya Sharma <87682688+Sharma8aditya@users.noreply.github.com>
2023-01-08 13:25:25 +0530

    Time: 6 ms (91.95%), Space: 7.4 MB (15.65%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        unordered_map<char, int> mp1, mp2;
+        for(char i: s){
+            mp1[i]++;
+        }
+        
+        for(char i: t){
+            mp2[i]++;
+        }
+        
+        for(int i = 0;i<s.size();i++){
+            if(mp1[s[i]] != mp2[s[i]]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
