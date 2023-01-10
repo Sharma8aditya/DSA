949f14a9d24ded3c065aba1eee56fdff95a2bb7d
Aditya Sharma <87682688+Sharma8aditya@users.noreply.github.com>
2023-01-10 11:04:48 +0530

    Time: 3 ms (65.62%), Space: 10.1 MB (48.63%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,7 +12,7 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p== NULL && q == NULL)
+       if(p== NULL && q == NULL)
             return true;
         else if(p == NULL || q == NULL)
             return false;
