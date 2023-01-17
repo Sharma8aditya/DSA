d1c565d4d038cccd4a4e1dbd9a1d52bdac52a87e
Aditya Sharma <87682688+Sharma8aditya@users.noreply.github.com>
2023-01-17 16:00:41 +0530

    Time: 22 ms (89.87%), Space: 11.1 MB (52.66%) - LeetHub

---
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minFlipsMonoIncr(string s) {
+        int count_one = 0, count_flip = 0;
+        for(auto i: s){
+            if(i == '1'){
+                count_one++;
+            }
+            else{
+                count_flip++;
+            }
+            count_flip = min(count_one, count_flip);
+        }
+        return count_flip;
+    }
+};
