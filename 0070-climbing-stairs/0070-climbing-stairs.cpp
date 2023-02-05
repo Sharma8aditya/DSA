e0067b54f4a8233b18e69f82428389c4d3022fc3
Aditya Sharma <87682688+Sharma8aditya@users.noreply.github.com>
2023-02-05 14:12:52 +0530

    Time: 0 ms (100.00%), Space: 6 MB (42.15%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        //same approach as fibonacci numbers
+        int prev = 1;
+        int prev2 = 1;
+        
+        for(int i = 2;i<=n;i++){
+            int curi = prev + prev2;
+            prev2 = prev;
+            prev = curi;
+        }
+        return prev;
+    }
+};
