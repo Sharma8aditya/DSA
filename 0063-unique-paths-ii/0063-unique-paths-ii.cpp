2b570d74632be0f28afce404a2c4299e45570841
Aditya Sharma <87682688+Sharma8aditya@users.noreply.github.com>
2023-02-26 11:45:27 +0530

    Time: 7 ms (32.41%), Space: 7.7 MB (84.00%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        int dp[m][n];
+        
+        for(int i = 0;i<m;i++){
+            for(int j = 0;j<n;j++){
+                if(i == 0 && j == 0 && obstacleGrid[i][j] != 1){
+                    dp[i][j] = 1;
+                    continue;
+                }
+                else if(obstacleGrid[i][j] == 1){
+                    dp[i][j] = 0;
+                    continue;
+                }
+                int up = 0, left = 0;
+                if(i>0) up = dp[i-1][j];
+                if(j>0) left = dp[i][j-1];
+                
+                dp[i][j] = up + left;
+            }
+            
+        }
+        return dp[m-1][n-1];
+        
+    }
+};
