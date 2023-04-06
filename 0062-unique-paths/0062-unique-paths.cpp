ec9f9dd0a4cd2edb9ae5b7868cfe1fed5bc15f48
Aditya Sharma <87682688+Sharma8aditya@users.noreply.github.com>
2023-04-06 17:14:49 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (89.88%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    
-    //tabulation approach
     int uniquePaths(int m, int n) {
-        int dp[m][n];
+        int  dp[m][n];
         for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
-                if(i == 0 && j == 0){
+                if(i == 0 && j == 0){               //base case
                     dp[i][j] = 1;
-                }else{
+                }
+                else{
                     int up = 0, left = 0;
-                    if(i>0) up = dp[i-1][j];             //when row decreases
-                    if(j>0) left = dp[i][j-1];           //when column decreases
+                    if(i>0) up = dp[i-1][j];              //moving up in row
+                    if(j>0) left = dp[i][j-1];            //moving left in column
                     dp[i][j] = up + left;
                 }
-                
             }
         }
-        return dp[m-1][n-1];
+        return dp[m-1][n-1];                              //start from bottom-right
     }
 };
