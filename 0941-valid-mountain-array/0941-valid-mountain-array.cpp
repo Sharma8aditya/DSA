8d542ca227d29fc57327832e3da1b7b86e9b0544
Aditya Sharma <87682688+Sharma8aditya@users.noreply.github.com>
2023-04-18 17:21:39 +0530

    Time: 37 ms (18.33%), Space: 22.5 MB (8.13%) - LeetHub

--- 0941-valid-mountain-array/0941-valid-mountain-array.cpp
+++ 0941-valid-mountain-array/0941-valid-mountain-array.cpp
@@ -3,19 +3,21 @@ public:
     bool validMountainArray(vector<int>& arr) {
         int n = arr.size();
         int i = 0;
-        while(i<n && i+1<n && arr[i] < arr[i+1]){         //increasing part
+        while(i<n-1 && arr[i] < arr[i+1]){          //strictly incresing condition
             i++;
         }
         
-        if(i ==0 || i+1 >= n){         //i == 0 nvere increasing i+1 > n last of array
+        //check if i is not at start or end of the array
+        if(i==0 || i == n-1){
             return false;
         }
         
-        while(i<n && i+1 < n){
-            if(arr[i] <= arr[i++ + 1]){         //not striclty decreasing
-                 return false;
-            }
+        //check for stricty decreasing 
+        while(i<n-1 && arr[i] > arr[i+1]){
+            i++;
         }
-        return true;
+        
+        //check if i reaches the end of the array or not
+        return(i==n-1);
     }
 };
