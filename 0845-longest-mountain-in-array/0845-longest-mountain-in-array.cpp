d8b4a9d7f5cc3e68cdbff715a162f14223383182
Aditya Sharma <87682688+Sharma8aditya@users.noreply.github.com>
2023-04-18 18:03:54 +0530

    Time: 20 ms (67.35%), Space: 18.5 MB (69.02%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
@@ -4,7 +4,7 @@ public:
         int n = arr.size();
         //move from 1 to n-2  because last element can't be the peak in the array
         int ans = 0;
-        for(int i = 1; i<=n-2;){
+        for(int i = 1; i<n-1;){
             if(arr[i] > arr[i+1] && arr[i] > arr[i-1]){      //peak condition
                 int count = 1;           //1 to count peak also
                 int j = i;
