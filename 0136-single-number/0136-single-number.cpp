803debde997d3011ffae8edd11d73d980a139448
Aditya Sharma <87682688+Sharma8aditya@users.noreply.github.com>
2023-05-09 05:34:28 +0530

    Time: 16 ms (83.78%), Space: 16.9 MB (52.30%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int xor1 = nums[0];
+        int ans = nums[0];
         for(int i = 1;i<nums.size();i++){
-            xor1 ^= nums[i];
+            ans = ans^nums[i];
         }
-        return xor1;
+        return ans;
     }
 };
