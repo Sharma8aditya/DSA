fc5bb0244c1c97e9461304a596560f8e3f0e9804
Aditya Sharma <87682688+Sharma8aditya@users.noreply.github.com>
2023-06-07 18:05:08 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (41.95%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> arr;
-        vector<int> temp;
-        temp.push_back(1);
-        if (numRows >= 1) arr.push_back(temp);
-        if (numRows >= 2) {
-            temp.push_back(1);
-            arr.push_back(temp);
+    vector<int> ncr(int n){
+        long long ans = 1;
+        vector<int> store;
+        store.push_back(1);
+        for(int i = 1;i<n;i++){
+            ans = ans * (n-i);
+            ans = ans/i;
+            store.push_back(ans);
         }
-        if (numRows > 2)
-        {
-            for (int j=2; j<numRows; j++){
-                vector<int> next_temp;
-                next_temp.push_back(1);
-                for (int i=1; i<temp.size(); i++)
-                {
-                    next_temp.push_back(temp[i-1]+temp[i]);
-                }
-                next_temp.push_back(1);
-                temp=next_temp;
-                arr.push_back(temp);
-            }
+        return store;
+    }
+    
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>> res;
+        for(int i = 1;i<=n;i++){
+            res.push_back(ncr(i));
         }
-        return arr;
+        return res;
     }
 };
