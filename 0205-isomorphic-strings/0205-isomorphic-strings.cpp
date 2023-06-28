1e8536665a499bd36aff7c34e13c3057697a2cd2
Aditya Sharma <87682688+Sharma8aditya@users.noreply.github.com>
2023-06-28 22:13:26 +0530

    Time: 8 ms (59.32%), Space: 6.8 MB (99.53%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map <char,int> mp1,mp2;
-        int n=s.size();
-        for(int i=0;i<n;i++){
-            if(mp1[s[i]]!=mp2[t[i]]) // If different values are assigned previously then Return False
+        unordered_map<char, int> mp1;
+        unordered_map<char, int> mp2;
+        int n = s.length();
+        
+//         for(auto it: s){
+//             mp1[it]++;
+//         }
+        
+//         for(auto it: t){
+//             mp2[it]++;
+            
+//         }
+        
+        for(int i = 0;i<n;i++){
+            if(mp1[s[i]] != mp2[t[i]]){
                 return false;
-            mp1[s[i]]=i+1; // Assigning the Freq/Values to the letters
-            mp2[t[i]]=i+1;
+            }
+            mp1[s[i]] = i+1;
+            mp2[t[i]] = i+1;
         }
-        return true; // When the letter are Isomorphic and satisfy all the conditions
+        return true;
     }
 };
